7cfafec903fcbc67e4e9c8eed42204b7066f1d0a
Cole Roxstrom <102431411+liquidprototypes@users.noreply.github.com>
2022-12-21 11:08:21 -0800

    Add files via upload
    print("Welcome to Band Animal Name Generator")
    city = input("What city did you grow up in? ")
    pet = input("What was the name of your last pet? ")
    print(city + " " + pet)

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+print("Welcome to Band Animal Name Generator")
+city = input("What city did you grow up in? ")
+pet = input("What was the name of your last pet? ")
+print(city + " " + pet) 
